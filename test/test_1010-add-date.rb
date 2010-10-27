212844c911b1f54862743249d36b58fb0370e4a8
Dariusz Synowiec <darek129@gmail.com>
2010-10-27 11:38:22 +0200

    add + time operation added

--- test/test_1010-add-date.rb
+++ test/test_1010-add-date.rb
@@ -27,6 +27,21 @@ export TODO_FILE=\"$TODO_DIR/todo.txt\"
       assert_equal(['1 ' + Time.now.strftime('%Y-%m-%d') + ' ' + tasks[0]], @rtodo.ls)
    end
 
+   def test_add_list_time_from_cfg
+      #add auto parse option
+      open(@dotFname, 'a') { |f|
+            f.puts "export TODOTXT_DATE_ON_ADD=1"
+      }
+
+      #reparse dotfile
+      @rtodo = Rtodo.new({:dotfile => @dotFname})
+
+      tasks = ['conquer the World']
+      assert_equal('1 ' + Time.now.strftime('%Y-%m-%d') + ' ' + tasks[0], @rtodo.add(tasks[0]))
+
+      assert_equal(['1 ' + Time.now.strftime('%Y-%m-%d') + ' ' + tasks[0]], @rtodo.ls)
+   end
+
    def teardown
       FileUtils.remove_entry_secure @tmpdir
       FileUtils.remove_entry_secure @dotFname
@@ -44,48 +59,6 @@ a date to each item.
 '
 . ./test-lib.sh
 
-#
-# Add and list
-#
-test_todo_session 'cmd line first day' <<EOF
->>> todo.sh -t add notice the daisies
-1 2009-02-13 notice the daisies
-TODO: 1 added.
-
->>> todo.sh list
-1 2009-02-13 notice the daisies
---
-TODO: 1 of 1 tasks shown
-EOF
-
-test_tick
-
-test_todo_session 'cmd line second day' <<EOF
->>> todo.sh -t add smell the roses
-2 2009-02-14 smell the roses
-TODO: 2 added.
-
->>> todo.sh list
-1 2009-02-13 notice the daisies
-2 2009-02-14 smell the roses
---
-TODO: 2 of 2 tasks shown
-EOF
-
-test_tick
-
-test_todo_session 'cmd line third day' <<EOF
->>> todo.sh -t add mow the lawn
-3 2009-02-15 mow the lawn
-TODO: 3 added.
-
->>> todo.sh list
-1 2009-02-13 notice the daisies
-2 2009-02-14 smell the roses
-3 2009-02-15 mow the lawn
---
-TODO: 3 of 3 tasks shown
-EOF
 
 # Switch to config file
 echo "export TODOTXT_DATE_ON_ADD=1" >> todo.cfg
